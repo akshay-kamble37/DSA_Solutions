ca494b8fd108db13e022d0d374595eecb1548f40
Akshay <akamble97605@gmail.com>
2026-04-24 19:57:37 +0530

    Added Arrays solutions

---
+++ Arrays/Two_sum.cpp
@@ -0,0 +1,21 @@
+// Problem: Two Sum
+// Platform: LeetCode
+// Difficulty: Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+
+        if (mp.find(diff) != mp.end()) {
+            return {mp[diff], i};
+        }
+
+        mp[nums[i]] = i;
+    }
+    return {};
+}
