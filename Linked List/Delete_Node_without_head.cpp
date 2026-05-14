8dfa855378f966084018896b0fa70fff999ceed3
Akshay <akamble97605@gmail.com>
2026-05-14 23:57:14 +0530

    Delete given node without head

---
+++ Linked List/Delete_Node_without_head.cpp
@@ -0,0 +1,19 @@
+//Problem: Delete given node without head
+//Difficulty: Medium 
+//Platform : GFG/Leetcode
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
