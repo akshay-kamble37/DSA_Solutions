0c89c0169754dfab31edb783dd4d520889b1923f
akshay-kamble37 <akamble97605@gmail.com>
2026-07-26 00:11:35 +0530

    Time: 12 ms (83.68%), Space: 26.1 MB (38.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string,vector<string>> mp;
+        unordered_map<string,vector<string>> mp;
         for(int i=0;i<strs.size();i++){
             string orignal=strs[i];
             string st = orignal;
